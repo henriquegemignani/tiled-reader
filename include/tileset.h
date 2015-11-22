2962ca4f4a1a1f328fefb254e93c390a00c88cbc
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2015-11-22 00:08:31 -0200

    Implement tileset constructor.

--- include/tileset.h
+++ include/tileset.h
@@ -4,6 +4,7 @@
 #include "fileloader.h"
 #include <string>
 #include <memory>
+#include <unordered_map>
 
 class JSONNode;
 
@@ -13,6 +14,16 @@ class Tileset {
 public:
 	Tileset(const JSONNode& json);
 	static std::unique_ptr<Tileset> ReadFromFile(const std::string&, const FileLoader& loader);
+
+private:
+	std::string image_;
+	int tile_width_;
+	int tile_height_;
+	int image_width_;
+	int image_height_;
+	int margin_;
+	int spacing_;
+	std::unordered_map<std::string, std::string> properties_;
 };
 
 } // namespace tiled
--- src/tileset.cc
+++ src/tileset.cc
@@ -1,9 +1,24 @@
 #include "tileset.h"
 
+#include <libjson.h>
+
 namespace tiled {
 
-Tileset::Tileset(const JSONNode& json) {
-	throw "NYI";
+Tileset::Tileset(const JSONNode& json_node) {
+	image_ = json_node["image"].as_string();
+	tile_width_ = json_node["tilewidth"].as_int();
+	tile_height_ = json_node["tileheight"].as_int();
+	image_width_ = json_node["imagewidth"].as_int();
+	image_height_ = json_node["imageheight"].as_int();
+	margin_ = json_node["margin"].as_int();
+	spacing_ = json_node["spacing"].as_int();
+
+	try {
+		for (const auto& property_node : json_node.at("properties")) {
+			properties_[property_node.name()] = property_node.as_string();
+		}
+	}
+	catch (std::out_of_range) {}
 }
 
 std::unique_ptr<Tileset> Tileset::ReadFromFile(const std::string&, const FileLoader& loader) {
